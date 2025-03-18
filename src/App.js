6270d2e755d92403ff6450149f13d53d42eeac80
PAInPROCk <2601prathmeshbhadwalkar@gmail.com>
2025-03-18 15:21:52 +0530

    18-3-2025 3:20 CWH v20 R 7:18

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     setTimeout(() => setAlert(null), 1500);
   }
 
-  const toggleMode = () => {
+  const toggleMode = (cls) => {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
